0ed24b41cb04a76a8e5c1ec7b9d00865634a86ae
Mark Jones <1mark1w1jones1@gmail.com>
2022-12-12 17:59:22 -0500

    added blockletters.py first lesson

---
+++ blockletters/initials.py
@@ -0,0 +1,12 @@
+
+# Mark Jones
+# A fun fact about me
+
+# Task was to complete blockinitials in block letters
+print("M   M     J")
+print("MM MM     J")
+print("M M M     J")
+print("M   M     J")
+print("M   M J   J")
+print("M   M J   J")
+print("M   M JJJJJ")
